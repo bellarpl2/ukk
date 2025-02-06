9532a2912d56a550a0e048d49812e800cab29322
bellarpl2 <140518361+bellarpl2@users.noreply.github.com>
2025-02-07 04:47:14 +0700

    Create Colors.dart

---
+++ Colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryPurple = Color(0xFF6B2FBC);
+  static const Color lightPurple = Color(0xFFE5D4F7);
+static const Color darkPurple = Color(0xFF4A1E96);
+}
